c6336dbb687d1d92aca7585642d5dc48bfd1e88b
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 16:12:11 -0700

    Would you like to play again?

--- game.py
+++ game.py
@@ -2,19 +2,28 @@
 name = raw_input("Welcome! What's your name?")
 
 from random import randint
-number = randint(1,100)
 
+#greeting 
 print "Howdy, %s! I'm thinking of a number between 1 and 100." %(str(name))
 
+#function that chooses a number between 1 and 100
+def pick_number():
+    number = randint(1,100)
+    return number
+
+number = pick_number()
+
+#function that asks for a guess and checks whether it's an integer 
 def new_guess():
     while True:
         try:
-            guess = int(raw_input("What's your guess?"))
+            guess = int(raw_input("What's your guess? "))
             break
         except ValueError:
             print "Dummy! That's not an integer! Try again."
     return guess 
 
+#feedback on guesses until game is won
 while True: 
     guess = new_guess()
     if guess < 1 or guess > 100:
@@ -25,4 +34,10 @@ while True:
         print "You guessed too low!"
     else:
         print "You guessed it! Good job!"
-        break
+        play_again = raw_input("Would you like to play again? y/n: ")
+        if play_again == 'y':
+            print "I've picked a new number."
+            number = pick_number()
+        else:
+            print "Thanks for playing!"
+            break
